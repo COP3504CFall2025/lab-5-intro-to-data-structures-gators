3b7e84ed6568fcc336935dd63df48690dfd5f717
Ryan Samuels <rjs.samuels@gmail.com>
2025-11-07 17:17:59 -0500

    Finished LinkedList and ABDQ

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -48,39 +48,71 @@ public:
 	// Insertion
 	void addHead(const T& data) {
 		Node<T>* new_node = new Node<T>{data, nullptr, head};
-		head = std::move(new_node);
+		if (head == nullptr) {
+			head = std::move(new_node);
+			tail = head;
+		} else {
+			new_node->next = head;
+			head->prev = new_node;
+			head = std::move(new_node);
+		}
 		count++;
 	}
 	void addTail(const T& data) {
 		Node<T>* new_node = new Node<T>{data, tail, nullptr};
-		tail->next = new_node;
-		tail = std::move(new_node);
+		if (tail == nullptr) {
+			tail = std::move(new_node);
+			head = tail;
+		} else {
+			new_node->prev = tail;
+			tail->next = new_node;
+			tail = std::move(new_node);
+		}
 		count++;
 	}
 
 	// Removal
 	bool removeHead() {
+		if (head == nullptr) {
+			return false;
+		}
+		if (head == tail) {
+			delete head;
+			head = nullptr;
+			tail = nullptr;
+			count--;
+			return true;
+		}
 		Node<T>* next_head = head->next;
 		delete head;
 		head = std::move(next_head);
+		head->prev = nullptr;
 		count--;
 		return true;
 	}
 	bool removeTail() {
+		if (tail == nullptr) {
+			return false;
+		}
+		if (head == tail) {
+			delete tail;
+			head = nullptr;
+			tail = nullptr;
+			count--;
+			return true;
+		}
 		Node<T>* next_tail = tail->prev;
 		delete tail;
 		tail = std::move(next_tail);
+		tail->next = nullptr;
 		count--;
 		return true;
 	}
 	void Clear() {
-		Node<T>* ptr = head;
-		while (ptr != nullptr) {
-			Node<T>* ptr_next = ptr->next;
-			delete ptr;
-			ptr = std::move(ptr_next);
+		bool val = true;
+		while (val) {
+			val = this->removeHead();
 		}
-		count = 0;
 	}
 
 	// Operators
@@ -100,12 +132,11 @@ public:
 	LinkedList<T>& operator=(const LinkedList<T>& rhs) {
 		if (this == &rhs) return *this;
         this->Clear();
-
 		Node<T>* ptr = rhs.head;
 		this->addHead(ptr->data);
-		while (ptr != nullptr) {
+		while (ptr->next != nullptr) {
 			ptr = ptr->next;
-			Node<T>* new_node = new Node(ptr->data, ptr->prev, ptr->next);
+			Node<T>* new_node = new Node{ptr->data, ptr->prev, ptr->next};
 			head->next = new_node;
 			tail = new_node;
 		}
@@ -117,20 +148,17 @@ public:
 	// Construction/Destruction
 	LinkedList() : head(nullptr), tail(nullptr), count(0) {}
 	LinkedList(const LinkedList<T>& list) {
-		this->Clear();
-		Node<T>* ptr = list.head;
-		this->addHead(ptr->data);
-		while (ptr != nullptr) {
-			ptr = ptr->next;
-			Node<T>* new_node = new Node(ptr->data, ptr->prev, ptr->next);
-			head->next = new_node;
-			tail = new_node;
+		count = 0;
+		head = nullptr;
+		tail = nullptr;
+		Node<T>* otherptr = list.head;
+		while (otherptr != nullptr) {
+			this->addTail(otherptr->data);
+			otherptr = otherptr->next;
 		}
 		count = list.count;
 	}
 	LinkedList(LinkedList<T>&& other) noexcept {
-		this->Clear();
-
         head = std::move(other.head);
 		other.head = nullptr;
 		tail = std::move(other.tail);
