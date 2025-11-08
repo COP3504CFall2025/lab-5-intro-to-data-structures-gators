3a158186e977bce07bed06bab4f33efddea1a386
Ryan Samuels <rjs.samuels@gmail.com>
2025-11-07 22:02:09 -0500

    holy changes

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -25,34 +25,32 @@ public:
     explicit ABDQ(std::size_t capacity) : capacity_(capacity), size_(0), front_(0), back_(0) {
         data_ = new T[capacity_];
     }
-    ABDQ(const ABDQ& other) {
-        size_ = other.size_;
-        capacity_ = other.capacity_;
+    ABDQ(const ABDQ& other) : capacity_(other.capacity_), size_(other.size_), front_(0), back_(other.size_) {
         data_ = new T[other.capacity_];
-        for (std::size_t i = 0; i < other.size_; i++) {
-            this->data_[i] = other.data_[i];
+        for (std::size_t i = 0; i < size_; ++i) {
+            data_[i] = other.data_[(other.front_ + i) % other.capacity_];
         }
     }
-    ABDQ(ABDQ&& other) noexcept {
-        data_ = other.data_;
-        size_ = other.size_;
-        capacity_ = other.capacity_;
+    ABDQ(ABDQ&& other) noexcept : data_(other.data_), capacity_(other.capacity_), size_(other.size_), front_(other.front_), back_(other.back_){
         other.data_ = nullptr;
         other.size_ = 0;
         other.capacity_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
     }
     ABDQ& operator=(const ABDQ& other) {
         if (this == &other) return *this;
         delete[] data_;
 
-        T* data = new T[other.capacity_];
-        size_ = other.size_;
-        capacity_ = other.capacity_;
-        data_ = data;
-
-        for (std::size_t i = 0; i < other.size_; i++) {
-            this->data_[i] = other.data_[i];
+        T* data_ = new T[other.capacity_];
+        for (std::size_t i = 0; i < other.size_; ++i) {
+            data_[i] = other.data_[(other.front_ + i) % other.capacity_];
         }
+        size_ = other.size_;
+        front_ = 0;
+        back_ = size_;
+        capacity_ = other.capacity_;
+
         return *this;
     }
     ABDQ& operator=(ABDQ&& other) noexcept {
@@ -62,11 +60,15 @@ public:
         data_ = other.data_;
         size_ = other.size_;
         capacity_ = other.capacity_;
+        front_ = other.front_;
+        back_  = other.back_;
 
         other.data_ = nullptr;
-        other.size_ = 0;
         other.capacity_ = 0;
-
+        other.size_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+        
         return *this;
     }
     ~ABDQ() noexcept {
@@ -79,7 +81,7 @@ public:
     // Insertion
     void pushFront(const T& item) override {
         if (size_ == capacity_ - 1) {
-            throw std::runtime_error("Full Data Structure!");
+            ensureCapacity();
         }
         front_ = (front_ - 1 + capacity_) % capacity_;
         data_[front_] = item;
@@ -87,7 +89,7 @@ public:
     }
     void pushBack(const T& item) override {
         if (size_ == capacity_ - 1) {
-            throw std::runtime_error("Full Data Structure!");
+            ensureCapacity();
         }
         data_[back_] = item;
         back_ = (back_ + 1) % capacity_;
@@ -103,6 +105,7 @@ public:
         T value = data_[front_];
         front_ = (front_ + 1) % capacity_;
         size_ = size_ - 1;
+        shrinkIfNeeded();
         return value;
     }
     T popBack() override {
@@ -112,21 +115,35 @@ public:
         back_ = (back_ - 1 + capacity_) % capacity_;
         T value = data_[back_];
         size_ = size_ - 1;
+        shrinkIfNeeded();
         return value;
     }
 
     void ensureCapacity() {
-        capacity_ *= SCALE_FACTOR;
+        std::size_t new_capacity_ = capacity_ * SCALE_FACTOR;
+        T* data_2 = new T[new_capacity_];
+        for (std::size_t i = 0; i < size_; ++i) {
+            data_2[i] = data_[(front_ + i) % capacity_];
+        }
+        delete[] data_;
+        data_ = data_2;
+        capacity_ = new_capacity_;
+        front_ = 0;
+        back_  = size_;
     }
 
     void shrinkIfNeeded() {
-        if (size_ <= capacity_/SCALE_FACTOR) {
-            capacity_ /= SCALE_FACTOR;
-            T* data_2 = new T[capacity_];
-            for (std::size_t i = 0; i < size_; i++) {
-                data_2[i] = data_[i];
+        if (size_ * SCALE_FACTOR <= capacity_) {
+            std::size_t new_capacity_ = capacity_/SCALE_FACTOR;
+            T* data_2 = new T[new_capacity_];
+            for (std::size_t i = 0; i < size_; ++i) {
+                data_2[i] = data_[(front_ + i) % capacity_];
             }
-            data_ = std::move(data_2);
+            delete[] data_;
+            data_ = data_2;
+            capacity_ = new_capacity_;
+            front_ = 0;
+            back_  = size_;
         }
     }
 
@@ -153,13 +170,13 @@ public:
         if (size_ == 0) {
             throw std::runtime_error("Empty Data Structure!");
         }
-        return front_;
+        return data_[front_];
     }
     const T& back() const override {
         if (size_ == 0) {
             throw std::runtime_error("Empty Data Structure!");
         }
-        return back_;
+        return data_[(back_ + capacity_ - 1) % capacity_];
     }
 
     // Getters
