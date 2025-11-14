37dc85d7537cf3caae6509d48c4264dbe2725f2d
Benjamin Charbonneau <118695671+Br-Charb@users.noreply.github.com>
2025-11-14 02:39:47 +0000

    Linklist and ABS

--- ABS.hpp
+++ ABS.hpp
@@ -11,29 +11,102 @@ template<typename T>
 class ABS : public StackInterface<T> {
 public:
     // Big 5 + Parameterized Constructor
-    ABS();
-    explicit ABS(const size_t capacity);
-    ABS(const ABS& other);
-    ABS& operator=(const ABS& rhs);
-    ABS(ABS&& other) noexcept;
-    ABS& operator=(ABS&& rhs) noexcept;
-    ~ABS() noexcept override;
+    ABS(){
+        array_ = new T[1];
+        capacity_ = 1;
+        curr_size_ = 0;
+    }
+    explicit ABS(const size_t capacity){
+        array_ = new T[capacity];
+        capacity_ = capacity;
+        curr_size_ = 0;
+    }
+    ABS(const ABS& other){
+        this->array_ = new T[other.capacity_];
+        this->capacity_ = other.capacity_;
+        this->curr_size_ = other.curr_size_;
+        for (int i = 0; i < other.curr_size_; i++){
+            this->array_[i] = other.array_[i];
+        }
+    }
+    ABS& operator=(const ABS& rhs){
+        if (this == &rhs){
+            return *this;
+        }
+        delete[] this->array_;
+        this->array_ = new T[rhs.capacity_];
+        this->capacity_ = rhs.capacity_;
+        this->curr_size_ = rhs.curr_size_;
+        for (int i = 0; i < rhs.curr_size_; i++){
+            this->array_[i] = rhs.array_[i];
+        }
+        return *this;
+    }
+    ABS(ABS&& other) noexcept{
+        this->array_ = other.array_;
+        this->capacity_ = other.capacity_;
+        this->curr_size_ = other.curr_size_;
+        other.array_ = nullptr;
+        other.capacity_ = 0;
+        other.curr_size_ = 0;
+    }
+    ABS& operator=(ABS&& rhs) noexcept{
+        if (this == &rhs){
+            return *this;
+        }
+        delete[] this->array_;
+        this->array_ = rhs.array_;
+        this->capacity_ = rhs.capacity_;
+        this->curr_size_ = rhs.curr_size_;
+        rhs.array_ = nullptr;
+        rhs.capacity_ = 0;
+        rhs.curr_size_ = 0;
+        return *this;
+    }
+    ~ABS() noexcept override{
+        delete[] this->array_;
+        array_ = nullptr;
+        capacity_ = 0;
+        curr_size_ = 0;
+    }
 
     // Get the number of items in the ABS
-    [[nodiscard]] size_t getSize() const noexcept override;
+    [[nodiscard]] size_t getSize() const noexcept override { return curr_size_;}
 
     // Get the max size of the ABS
-    [[nodiscard]] size_t getMaxCapacity() const noexcept;
+    [[nodiscard]] size_t getMaxCapacity() const noexcept {return capacity_;}
 
     // Return underlying data for the stack
-    [[nodiscard]] T* getData() const noexcept;
+    [[nodiscard]] T* getData() const noexcept { return array_;}
 
     // Push item onto the stack
-    void push(const T& data) override;
+    void push(const T& data) override{
+        if (curr_size_ == capacity_){
+            if (capacity_ == 0) {capacity_ = 1;}
+            else { capacity_ *= scale_factor_;}
+            T* temp = new T[capacity_];
+            for (int i = 0; i < curr_size_; i++){
+                temp[i] = array_[i];
+            }
+            delete[] array_;
+            array_ = temp;
+        }
+        array_[curr_size_] = data;
+        curr_size_ += 1;
+    }
 
-    T peek() const override;
+    T peek() const override{
+        if (curr_size_ > 0){ return array_[curr_size_-1];}
+        throw std::runtime_error("ABS is empty");
+    }
 
-    T pop() override;
+    T pop() override{
+        if (curr_size_ == 0){
+            throw std::runtime_error("ABS is empty");
+        }
+        curr_size_ -= 1;
+        return array_[curr_size_];
+    }
 
 private:
     size_t capacity_;
--- LinkedList.hpp
+++ LinkedList.hpp
@@ -8,37 +8,162 @@ template <typename T>
 class LinkedList {
 public:
 	// Behaviors
-	void printForward() const;
-	void printReverse() const;
+	void printForward() const{
+		if (count == 0){
+			std::cout << "Empty Linked List" << std::endl;
+			return;
+		}
+		Node* copy = head;
+		while (copy) {
+			std::cout << (copy->data) << std::endl;
+			copy = copy->next;
+		}
+	}
+	void printReverse() const{
+		if (count == 0){
+			std::cout << "Empty Linked List" << std::endl;
+			return;
+		}
+		Node* copy = tail;
+		while (copy) {
+			std::cout << (copy->data) << std::endl;
+			copy = copy->prev;
+		}
+	}
 
 	// Accessors
-	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+	[[nodiscard]] unsigned int getCount() const {return count;}
+	Node* getHead(){return head;}
+	const Node* getHead() const {return head;}
+	Node* getTail() {return tail;}
+	const Node* getTail() const {return tail;}
 
 	// Insertion
-	void addHead(const T& data);
-	void addTail(const T& data);
+	void addHead(const T& data){
+		if (count != 0){
+			Node* newHead = new Node({data, nullptr, head});
+			head->prev = newHead;
+			head = newHead;
+		} else {
+			head = new Node({data, nullptr, nullptr});
+			tail = head;
+		}
+		count++;
+	}
+	void addTail(const T& data){
+		if (count != 0){
+			Node* newTail = new Node({data, tail, nullptr});
+			tail->next = newTail;
+			tail = newTail;
+		} else {
+			head = new Node({data, nullptr, nullptr});
+			tail = head;
+		}
+		count++;
+	}
 
 	// Removal
-	bool removeHead();
-	bool removeTail();
-	void Clear();
+	bool removeHead(){
+		if (count == 0){
+			return false;
+		} 
+		Node* prevHead = head;
+		if (count == 1){
+			head = nullptr;
+			tail = nullptr;
+		} else {
+			head = head->next;
+			head->prev = nullptr;
+		}
+		delete prevHead;
+		count--;
+		return true;
+	}
+	bool removeTail(){
+		if (count == 0){
+			return false;
+		} 
+		Node* prevTail = tail;
+		if (count == 1){
+			head = nullptr;
+			tail = nullptr;
+		} else {
+			tail = tail->prev;
+			tail->next = nullptr;
+		}
+		delete prevTail;
+		count--;
+		return true;
+	}
+	void Clear(){
+		while (removeHead()){}
+	}
 
 	// Operators
-	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept;
-	LinkedList<T>& operator=(const LinkedList<T>& rhs);
+	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept{
+		if (this == &other) {
+			return *this;
+		}
+		Clear();
+		this->head = other.head;
+		this->tail = other.tail;
+		this->count = other.count;
+		other.head = nullptr;
+		other.tail = nullptr;
+		other.count = 0;
+		return *this;
+	}
+
+	LinkedList<T>& operator=(const LinkedList<T>& rhs){
+		if (this == &rhs){
+			return *this;
+		}
+		Clear();
+		Node* copy = rhs.head;
+		while (copy) {
+			addTail(copy->data);
+			copy = copy->next;
+		}
+		return *this;
+	}
 
 	// Construction/Destruction
-	LinkedList();
-	LinkedList(const LinkedList<T>& list);
-	LinkedList(LinkedList<T>&& other) noexcept;
-	~LinkedList();
+	LinkedList(){
+		head = nullptr;
+		tail = nullptr;
+		count = 0;
+	}
+	LinkedList(const LinkedList<T>& list){
+		head = nullptr;
+		tail = nullptr;
+		count = 0;
+		Node* copy = list.head;
+		while (copy) {
+			addTail(copy->data);
+			copy = copy->next;
+		}
+	}
+	LinkedList(LinkedList<T>&& other) noexcept{
+		this->head = other.head;
+		this->tail = other.tail;
+		this->count = other.count;
+		other.head = nullptr;
+		other.tail = nullptr;
+		other.count = 0;
+	}
+	~LinkedList(){
+		Clear();
+	}
 
 private:
 	// Stores pointers to first and last nodes and count
+
+	struct Node {
+		T data;
+		Node* prev;
+		Node* next;
+	};
+
 	Node* head;
 	Node* tail;
 	unsigned int count;
